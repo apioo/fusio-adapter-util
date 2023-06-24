0f2b3b0f00d6b25cb5ed0620398f6272d64a471f
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-06-24 19:30:19 +0200

    update util cache

--- src/Action/UtilCache.php
+++ src/Action/UtilCache.php
@@ -23,11 +23,12 @@ namespace Fusio\Adapter\Util\Action;
 
 use Fusio\Engine\ActionAbstract;
 use Fusio\Engine\ContextInterface;
+use Fusio\Engine\Exception\ConfigurationException;
 use Fusio\Engine\Form\BuilderInterface;
 use Fusio\Engine\Form\ElementFactoryInterface;
 use Fusio\Engine\ParametersInterface;
-use Fusio\Engine\Request\HttpRequest;
-use Fusio\Engine\Request\RpcRequest;
+use Fusio\Engine\Request\HttpRequestContext;
+use Fusio\Engine\Request\RpcRequestContext;
 use Fusio\Engine\RequestInterface;
 use Psr\SimpleCache\CacheInterface;
 use PSX\Http\Environment\HttpResponseInterface;
@@ -53,12 +54,17 @@ class UtilCache extends ActionAbstract
 
     public function handle(RequestInterface $request, ParametersInterface $configuration, ContextInterface $context): HttpResponseInterface
     {
-        $key = $this->getCacheKey($request, $configuration);
-        $handler  = $this->getCacheHandler($this->connector->getConnection($configuration->get('connection')));
+        $action = $configuration->get('action');
+        if (empty($action)) {
+            throw new ConfigurationException('No action provided');
+        }
+
+        $key = $this->getCacheKey($request, $action);
+        $handler = $this->getCacheHandler($configuration->get('connection'));
         $response = $handler->get($key);
 
         if (empty($response)) {
-            $response = $this->processor->execute($configuration->get('action'), $request, $context);
+            $response = $this->processor->execute($action, $request, $context);
 
             $handler->set($key, $response, (int) $configuration->get('expire'));
         }
@@ -76,35 +82,37 @@ class UtilCache extends ActionAbstract
     /**
      * @psalm-suppress UndefinedClass
      */
-    private function getCacheHandler(mixed $connection): CacheInterface
+    private function getCacheHandler(?string $connection): CacheInterface
     {
         if (self::$handler) {
             return self::$handler;
         }
 
-        $handler = null;
-        if ($connection instanceof \Memcached) {
-            $handler = new MemcachedAdapter($connection);
-        } elseif ($connection instanceof \Redis) {
-            $handler = new RedisAdapter($connection);
+        if (empty($connection)) {
+            return self::$handler = $this->cache;
         }
 
-        if ($handler !== null) {
-            return self::$handler = new Psr16Cache($handler);
+        $instance = $this->connector->getConnection($connection);
+        if ($instance instanceof \Memcached) {
+            $handler = new MemcachedAdapter($instance);
+        } elseif ($instance instanceof \Redis) {
+            $handler = new RedisAdapter($instance);
         } else {
             return self::$handler = $this->cache;
         }
+
+        return self::$handler = new Psr16Cache($handler);
     }
 
-    private function getCacheKey(RequestInterface $request, ParametersInterface $configuration): string
+    private function getCacheKey(RequestInterface $request, string $action): string
     {
         $requestContext = $request->getContext();
-        if ($requestContext instanceof HttpRequest) {
-            return md5($configuration->get('action') . $requestContext->getMethod() . json_encode($requestContext->getUriFragments()) . json_encode($requestContext->getParameters()));
-        } elseif ($requestContext instanceof RpcRequest) {
-            return md5($configuration->get('action') . $requestContext->getMethod());
+        if ($requestContext instanceof HttpRequestContext) {
+            return md5($action . $requestContext->getRequest()->getMethod() . json_encode($requestContext->getParameters()) . json_encode($requestContext->getRequest()->getUri()->getParameters()));
+        } elseif ($requestContext instanceof RpcRequestContext) {
+            return md5($action . $requestContext->getMethod());
         } else {
-            return md5($configuration->get('action'));
+            return md5($action);
         }
     }
 }
